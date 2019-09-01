39141bfba977986a1234cf2a36cc1b0769381d2f
husthuaan <husthuaan@gmail.com>
2019-09-01 17:28:04 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # Attention on Attention for Image Captioning
-Code will be available soon.
+
+This repository includes the implementation for [Attention on Attention for Image Captioning](https://arxiv.org/abs/1908.06954) (to appear in ICCV 2019 as oral presentation).
+
+## Requirements
+```
+Python 3.6
+Java 1.8.0
+PyTorch 1.0
+cider (already been added as a submodule)
+coco-caption (already been added as a submodule)
+tensorboardX
+```
+
+## Training AoANet
+
+### Prepare data
+
+See details in `data/README.md`.
+
+### Start training
+
+```bash
+CUDA_VISIBLE_DEVICES=0 sh train.sh
+```
+
+See `opts.py` for the options.
+
+
+### Evaluation
+
+```bash
+$ CUDA_VISIBLE_DEVICES=0 python eval.py --model log/log_aoanet_rl/model.pth --infos_path log/log_aoanet_rl/infos_aoanet.pkl  --dump_images 0 --dump_json 1 --num_images -1 --language_eval 1 --beam_size 2 --batch_size 100 --split test
+```
+
+### Performance
+You will get the scores close to below after training under xe loss for 25 epochs:
+```
+{'Bleu_1': 0.7729384559899702, 'Bleu_2': 0.6163398035383025, 'Bleu_3': 0.4790123137715982, 'Bleu_4': 0.36944349063530374, 'METEOR': 0.2848188431924821, 'ROUGE_L': 0.5729849683867054, 'CIDEr': 1.1842173801790759, 'SPICE': 0.21650786258302354}
+```
+(**notes:** You can enlarge `--max_epochs` in `train.sh` to train the model for more epochs and improve the scores.)
+
+after training under SCST loss for another 15 epochs, you will get:
+```
+{'Bleu_1': 0.8054903453672397, 'Bleu_2': 0.6523038976984842, 'Bleu_3': 0.5096621263772566, 'Bleu_4': 0.39140307771618477, 'METEOR': 0.29011216375635934, 'ROUGE_L': 0.5890369750273199, 'CIDEr': 1.2892294296245852, 'SPICE': 0.22680092759866174}
+```
+
+
+## Reference
+
+If you find this repo useful, please consider citing:
+
+```
+@inproceedings{huang2019attention,
+  title={Attention on Attention for Image Captioning},
+  author={Huang, Lun and Wang, Wenmin and Chen, Jie and Wei, Xiao-Yong},
+  booktitle={International Conference on Computer Vision},
+  year={2019}
+}
+```
+
+## Acknowledgements
+
+This repository is based on [self-critical.pytorch](https://github.com/ruotianluo/self-critical.pytorch), and you may refer to it for more details about the code.
---
+++ dataloader.py
@@ -0,0 +1,396 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import h5py
+import os
+import numpy as np
+import random
+
+import torch
+import torch.utils.data as data
+
+import multiprocessing
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
+from functools import reduce
+
+class DataLoader(data.Dataset):
+
+    def reset_iterator(self, split):
+        del self._prefetch_process[split]
+        self._prefetch_process[split] = BlobFetcher(split, self, split=='train')
+        self.iterators[split] = 0
+
+    def get_vocab_size(self):
+        return self.vocab_size
+
+    def get_vocab(self):
+        return self.ix_to_word
+
+    def get_seq_length(self):
+        return self.seq_length
+
+    def __init__(self, opt):
+        self.opt = opt
+        self.batch_size = self.opt.batch_size
+        self.seq_per_img = opt.seq_per_img
+        
+        # feature related options
+        self.use_att = getattr(opt, 'use_att', True)
+        self.use_box = getattr(opt, 'use_box', 0)
+        self.norm_att_feat = getattr(opt, 'norm_att_feat', 0)
+        self.norm_box_feat = getattr(opt, 'norm_box_feat', 0)
+        self.no_lable = getattr(opt, 'test_server', 0)
+        self.test_val = getattr(opt, 'test_val', 0)
+
+        # load the json file which contains additional information about the dataset
+        print('DataLoader loading json file: ', opt.input_json)
+        self.info = json.load(open(self.opt.input_json))
+        if self.no_lable == 0:
+            self.ix_to_word = self.info['ix_to_word']
+            self.vocab_size = len(self.ix_to_word)
+            print('vocab size is ', self.vocab_size)
+        
+        # open the hdf5 file
+        print('DataLoader loading h5 file: ', opt.input_fc_dir, opt.input_att_dir, opt.input_box_dir)
+        if self.no_lable == 0:
+            self.h5_label_file = h5py.File(self.opt.input_label_h5, 'r', driver='core')
+
+        self.input_fc_dir = self.opt.input_fc_dir
+        self.input_att_dir = self.opt.input_att_dir
+        self.input_box_dir = self.opt.input_box_dir
+
+        # load in the sequence data
+        if self.no_lable == 0:            
+            seq_size = self.h5_label_file['labels'].shape
+            self.seq_length = seq_size[1]
+        else:
+            self.seq_length = 16
+        print('max sequence length in data is', self.seq_length)
+        if self.no_lable == 0:            
+        # load the pointers in full to RAM (should be small enough)
+            self.label_start_ix = self.h5_label_file['label_start_ix'][:]
+            self.label_end_ix = self.h5_label_file['label_end_ix'][:]
+
+            self.num_images = self.label_start_ix.shape[0]
+            print('read %d image features' %(self.num_images))
+
+        # separate out indexes for each of the provided splits
+        self.split_ix = {'train': [], 'val': [], 'test': []}
+        for ix in range(len(self.info['images'])):
+            img = self.info['images'][ix]
+            if self.test_val:
+                if 'val2014' in img['file_path']:
+                    self.split_ix['test'].append(ix)
+            else:
+                if img['split'] == 'train':
+                    self.split_ix['train'].append(ix)
+                elif img['split'] == 'val':
+                    self.split_ix['val'].append(ix)
+                elif img['split'] == 'test':
+                    self.split_ix['test'].append(ix)
+                elif opt.train_only == 0: # restval
+                    self.split_ix['train'].append(ix)
+
+        print('assigned %d images to split train' %len(self.split_ix['train']))
+        print('assigned %d images to split val' %len(self.split_ix['val']))
+        print('assigned %d images to split test' %len(self.split_ix['test']))
+
+        self.iterators = {'train': 0, 'val': 0, 'test': 0}
+        
+        self._prefetch_process = {} # The three prefetch process
+        for split in self.iterators.keys():
+            self._prefetch_process[split] = BlobFetcher(split, self, split=='train')
+            # Terminate the child process when the parent exists
+        def cleanup():
+            print('Terminating BlobFetcher')
+            for split in self.iterators.keys():
+                del self._prefetch_process[split]
+        import atexit
+        atexit.register(cleanup)
+
+    def get_captions(self, ix, seq_per_img):
+        # fetch the sequence labels
+        ix1 = self.label_start_ix[ix] - 1 #label_start_ix starts from 1
+        ix2 = self.label_end_ix[ix] - 1
+        ncap = ix2 - ix1 + 1 # number of captions available for this image
+        assert ncap > 0, 'an image does not have any label. this can be handled but right now isn\'t'
+
+        if ncap < seq_per_img:
+            # we need to subsample (with replacement)
+            seq = np.zeros([seq_per_img, self.seq_length], dtype = 'int')
+            for q in range(seq_per_img):
+                ixl = random.randint(ix1,ix2)
+                seq[q, :] = self.h5_label_file['labels'][ixl, :self.seq_length]
+        else:
+            ixl = random.randint(ix1, ix2 - seq_per_img + 1)
+            seq = self.h5_label_file['labels'][ixl: ixl + seq_per_img, :self.seq_length]
+
+        return seq
+
+    def get_batch(self, split, batch_size=None, seq_per_img=None):
+        batch_size = batch_size or self.batch_size
+        seq_per_img = seq_per_img or self.seq_per_img
+
+        fc_batch = [] # np.ndarray((batch_size * seq_per_img, self.opt.fc_feat_size), dtype = 'float32')
+        att_batch = [] # np.ndarray((batch_size * seq_per_img, 14, 14, self.opt.att_feat_size), dtype = 'float32')
+        if self.no_lable == 0:
+            label_batch = np.zeros([batch_size * seq_per_img, self.seq_length + 2], dtype = 'int')
+        mask_batch = np.zeros([batch_size * seq_per_img, self.seq_length + 2], dtype = 'float32')
+
+        wrapped = False
+
+        infos = []
+        # if self.no_lable == 0:
+        gts = []
+
+        for i in range(batch_size):
+            # fetch image
+            tmp_fc, tmp_att,\
+                ix, tmp_wrapped = self._prefetch_process[split].get()
+            fc_batch.append(tmp_fc)
+            att_batch.append(tmp_att)
+
+            if self.no_lable == 0:
+                label_batch[i * seq_per_img : (i + 1) * seq_per_img, 1 : self.seq_length + 1] = self.get_captions(ix, seq_per_img)
+
+            if tmp_wrapped:
+                wrapped = True
+
+            # Used for reward evaluation
+            if self.no_lable == 0:
+                gts.append(self.h5_label_file['labels'][self.label_start_ix[ix] - 1: self.label_end_ix[ix]])
+        
+            # record associated info as well
+            info_dict = {}
+            info_dict['ix'] = ix
+            info_dict['id'] = self.info['images'][ix]['id']
+            if self.no_lable == 0:
+                info_dict['file_path'] = self.info['images'][ix]['file_path']
+            infos.append(info_dict)
+
+        # #sort by att_feat length
+        # fc_batch, att_batch, label_batch, gts, infos = \
+        #     zip(*sorted(zip(fc_batch, att_batch, np.vsplit(label_batch, batch_size), gts, infos), key=lambda x: len(x[1]), reverse=True))
+        
+        if self.no_lable == 0:
+            fc_batch, att_batch, label_batch, gts, infos = \
+                zip(*sorted(zip(fc_batch, att_batch, np.vsplit(label_batch, batch_size), gts, infos), key=lambda x: 0, reverse=True))
+        else:  
+            fc_batch, att_batch, infos = \
+                zip(*sorted(zip(fc_batch, att_batch, infos), key=lambda x: 0, reverse=True))       
+            
+        data = {}
+        data['fc_feats'] = np.stack(reduce(lambda x,y:x+y, [[_]*seq_per_img for _ in fc_batch]))
+        # merge att_feats
+        max_att_len = max([_.shape[0] for _ in att_batch])
+        data['att_feats'] = np.zeros([len(att_batch)*seq_per_img, max_att_len, att_batch[0].shape[1]], dtype = 'float32')
+        for i in range(len(att_batch)):
+            data['att_feats'][i*seq_per_img:(i+1)*seq_per_img, :att_batch[i].shape[0]] = att_batch[i]
+        data['att_masks'] = np.zeros(data['att_feats'].shape[:2], dtype='float32')
+        for i in range(len(att_batch)):
+            data['att_masks'][i*seq_per_img:(i+1)*seq_per_img, :att_batch[i].shape[0]] = 1
+        # set att_masks to None if attention features have same length
+        if data['att_masks'].sum() == data['att_masks'].size:
+            data['att_masks'] = None
+        if self.no_lable == 0:
+            data['labels'] = np.vstack(label_batch)
+        # generate mask
+            nonzeros = np.array(list(map(lambda x: (x != 0).sum()+2, data['labels'])))
+            for ix, row in enumerate(mask_batch):
+                row[:nonzeros[ix]] = 1
+        data['masks'] = mask_batch
+        if self.no_lable == 0:
+            data['gts'] = gts # all ground truth captions of each images
+        data['bounds'] = {'it_pos_now': self.iterators[split], 'it_max': len(self.split_ix[split]), 'wrapped': wrapped}
+        data['infos'] = infos
+
+        data = {k:torch.from_numpy(v) if type(v) is np.ndarray else v for k,v in data.items()} # Turn all ndarray to torch tensor
+
+        return data
+
+    # It's not coherent to make DataLoader a subclass of Dataset, but essentially, we only need to implement the following to functions,
+    # so that the torch.utils.data.DataLoader can load the data according the index.
+    # However, it's minimum change to switch to pytorch data loading.
+    def __getitem__(self, index):
+        """This function returns a tuple that is further passed to collate_fn
+        """
+        ix = index #self.split_ix[index]
+        if self.use_att:
+            att_feat = np.load(os.path.join(self.input_att_dir, str(self.info['images'][ix]['id']) + '.npz'))['feat']
+            # Reshape to K x C
+            att_feat = att_feat.reshape(-1, att_feat.shape[-1])
+            if self.norm_att_feat:
+                att_feat = att_feat / np.linalg.norm(att_feat, 2, 1, keepdims=True)
+            if self.use_box:
+                box_feat = np.load(os.path.join(self.input_box_dir, str(self.info['images'][ix]['id']) + '.npy'))
+                # devided by image width and height
+                x1,y1,x2,y2 = np.hsplit(box_feat, 4)
+                h,w = self.info['images'][ix]['height'], self.info['images'][ix]['width']
+                box_feat = np.hstack((x1/w, y1/h, x2/w, y2/h, (x2-x1)*(y2-y1)/(w*h))) # question? x2-x1+1??
+                # box_feat = np.hstack((x1, y1, x2, y2, (x2-x1)*(y2-y1))) # question? x2-x1+1??
+                if self.norm_box_feat:
+                    box_feat = box_feat / np.linalg.norm(box_feat, 2, 1, keepdims=True)
+                att_feat = np.hstack([att_feat, box_feat])
+                # sort the features by the size of boxes
+                att_feat = np.stack(sorted(att_feat, key=lambda x:x[-1], reverse=True))
+        else:
+            att_feat = np.zeros((1,1,1))
+        return (np.load(os.path.join(self.input_fc_dir, str(self.info['images'][ix]['id']) + '.npy')),
+                att_feat,
+                ix)
+
+    def __len__(self):
+        return len(self.info['images'])
+
+class SubsetSampler(torch.utils.data.sampler.Sampler):
+    r"""Samples elements randomly from a given list of indices, without replacement.
+    Arguments:
+        indices (list): a list of indices
+    """
+
+    def __init__(self, indices):
+        self.indices = indices
+
+    def __iter__(self):
+        return (self.indices[i] for i in range(len(self.indices)))
+
+    def __len__(self):
+        return len(self.indices)
+
+
+def get_npy_data(image_id, loader):
+    # print('get_npy_data')
+    # return (np.load(fc_file),
+    #         np.load(att_file)['feat'],
+    #         image_id)
+    ix = image_id  # self.split_ix[index]
+    if loader.use_att:
+        att_feat = np.load(os.path.join(loader.input_att_dir, str(loader.info['images'][ix]['id']) + '.npz'))['feat']
+        # Reshape to K x C
+        att_feat = att_feat.reshape(-1, att_feat.shape[-1])
+        if loader.norm_att_feat:
+            att_feat = att_feat / np.linalg.norm(att_feat, 2, 1, keepdims=True)
+        if loader.use_box:
+            box_feat = np.load(os.path.join(loader.input_box_dir, str(loader.info['images'][ix]['id']) + '.npy'))
+            # devided by image width and height
+            x1, y1, x2, y2 = np.hsplit(box_feat, 4)
+            h, w = loader.info['images'][ix]['height'], loader.info['images'][ix]['width']
+            box_feat = np.hstack(
+                (x1 / w, y1 / h, x2 / w, y2 / h, (x2 - x1) * (y2 - y1) / (w * h)))  # question? x2-x1+1??
+            # box_feat = np.hstack((x1, y1, x2, y2, (x2-x1)*(y2-y1))) # question? x2-x1+1??
+            if loader.norm_box_feat:
+                box_feat = box_feat / np.linalg.norm(box_feat, 2, 1, keepdims=True)
+            att_feat = np.hstack([att_feat, box_feat])
+            # sort the features by the size of boxes
+            att_feat = np.stack(sorted(att_feat, key=lambda x: x[-1], reverse=True))
+    else:
+        att_feat = np.zeros((1, 1, 1))
+    return (np.load(os.path.join(loader.input_fc_dir, str(loader.info['images'][ix]['id']) + '.npy')),
+            att_feat,
+            ix)
+class BlobFetcher():
+    """Experimental class for prefetching blobs in a separate process."""
+    def __init__(self, split, dataloader, if_shuffle=False):
+        """
+        db is a list of tuples containing: imcrop_name, caption, bbox_feat of gt box, imname
+        """
+        self.split = split
+        self.dataloader = dataloader
+        self.if_shuffle = if_shuffle
+
+        # self.pool = Pool(2)
+        self.pool = ThreadPoolExecutor(max_workers=4)
+        # self.read_file_pool = Pool(4)
+        self.fifo = []
+        self.cur_idx = self.dataloader.iterators[self.split]  # index for loading features
+        self.cur_split_ix = self.dataloader.split_ix[self.split][:]  # copy
+
+    def terminate(self):
+        while len(self.fifo) > 0:
+            self.fifo.pop(0).result()
+        self.pool.shutdown()
+        # self.read_file_pool.terminate()
+        print(self.split, 'terminated')
+
+    def join(self):
+        # self.pool.join()
+        # self.read_file_pool.join()
+        print(self.split, 'joined')
+    def reset(self):
+        if len(self.fifo) == 0:
+            self.cur_idx = self.dataloader.iterators[self.split]
+            self.cur_split_ix = self.dataloader.split_ix[self.split][:]  # copy
+
+        for i in range(512 - len(self.fifo)):
+            image_id = self.cur_split_ix[self.cur_idx]
+
+            if self.cur_idx + 1 >= len(self.cur_split_ix):
+                self.cur_idx = 0
+                if self.if_shuffle:
+                    random.shuffle(self.cur_split_ix)
+            else:
+                self.cur_idx += 1
+            _image_id = self.dataloader.info['images'][image_id]['id']
+            self.fifo.append(self.pool.submit(get_npy_data, *(image_id, self.dataloader)))
+            # self.fifo.append(self.pool.apply_async(shit))
+    def _get_next_minibatch_inds(self):
+        max_index = len(self.cur_split_ix)
+        wrapped = False
+
+        ri = self.dataloader.iterators[self.split]  # index for reading features.
+        image_id = self.dataloader.split_ix[self.split][ri]
+        # print(ri)
+        # print(self.dataloader.split_ix[self.split])
+        # ix = self.dataloader.split_ix[self.split][ri]
+
+        ri_next = ri + 1
+        if ri_next >= max_index:
+            ri_next = 0
+            self.dataloader.split_ix[self.split] = self.cur_split_ix[:]  # copy
+            wrapped = True
+        self.dataloader.iterators[self.split] = ri_next
+
+        return image_id, wrapped
+
+    def get(self):
+        if len(self.fifo) < 100:
+            self.reset()
+
+        image_id, wrapped = self._get_next_minibatch_inds()
+        # print(image_id)
+        # print(self.fifo.pop(0))
+        # print(self.fifo.pop(0).wait())
+        tmp = self.fifo.pop(0).result()
+
+        # _image_id = self.dataloader.info['images'][image_id]['id']
+        # tmp = self.get_npy_data(image_id,os.path.join(self.dataloader.input_fc_dir,
+        #                        str(_image_id) + '.npy'),
+        #                        os.path.join(self.dataloader.input_att_dir,
+        #                 str(_image_id) + '.npz')
+        #                 )
+        att = tmp[1]
+        att = att.reshape(-1, tmp[1].shape[-1])
+        # print(att.shape)
+
+
+        # assert tmp[2] == image_id, "image_id not equal"
+
+        return (tmp[0], att, tmp[2],wrapped)
+import opts
+import time
+
+if __name__ == '__main__':
+    opt = opts.parse_opt()
+    loader = DataLoader(opt)
+
+    start = time.time()
+    for n in range(10):
+        print('---- batch: ' + str(n))
+        data = loader.get_batch('test')
+
+    start2 = time.time()
+    print('time: ', start2 - start)
+    # data = np.load('data/cocotalk_fc/93040.npy')
